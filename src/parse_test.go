c138c8fbc1ff7b862e1f393f66a7604ea45c9555
Samuel Tesla <stesla@engineyard.com>
2009-11-17 23:21:36 -0600

    Make test cases static instead of heap-allocated.

--- src/parse_test.go
+++ src/parse_test.go
@@ -6,16 +6,16 @@ import (
 
 func TestParse(t *testing.T) {
 	test(t, []testCase{
-		&parseTest{"ident - single chart", "x", "x"},
-		&parseTest{"ident - lowers", "foo", "foo"},
-		&parseTest{"ident - all", "aZ_1", "aZ_1"},
-		&parseTest{"abstraction", "fn x. y", "fn x. y"},
-		&parseTest{"application", "f x", "f x"},
-		&parseTest{"group", "(x)", "(x)"},
-		&parseTest{"Y combinator",
+		parseTest{"ident - single chart", "x", "x"},
+		parseTest{"ident - lowers", "foo", "foo"},
+		parseTest{"ident - all", "aZ_1", "aZ_1"},
+		parseTest{"abstraction", "fn x. y", "fn x. y"},
+		parseTest{"application", "f x", "f x"},
+		parseTest{"group", "(x)", "(x)"},
+		parseTest{"Y combinator",
             "fn g. (fn x. g (x x)) (fn x. g (x x))",
             "fn g. (fn x. g (x x)) (fn x. g (x x))"},
-		&parseTest{"var applied to abstraction", "x (fn y. y)", "x (fn y. y)"},
-		&parseTest{"currying", "fn f x. f x", "fn f. fn x. f x"}
+		parseTest{"var applied to abstraction", "x (fn y. y)", "x (fn y. y)"},
+		parseTest{"currying", "fn f x. f x", "fn f. fn x. f x"}
 	});
 }
